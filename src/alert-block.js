9cb7a12ba909464839ba4b1a0e499da2cf4bbd0e
Adam Kim <156630256+Kimaj0424@users.noreply.github.com>
2024-02-27 12:08:28 -0500

    Update alert-block.js

--- src/alert-block.js
+++ src/alert-block.js
@@ -4,6 +4,7 @@ export class AlertBlock extends LitElement {
   static get tag() {
     return "alert-block";
   }
+
   constructor() {
     super();
     this.close = false;
@@ -11,7 +12,7 @@ export class AlertBlock extends LitElement {
     this.date = "NOVEMBER 17, 2023 12:00 AM";
     this.status = "notice";
     this.link = "https://www.psu.edu/news";
-    this.message="Occaecat laboris incididunt ea labore quis in qui commodo velit cillum et commodo. Dolore consectetur eu eu reprehenderit anim fugiat in nostrud anim magna enim nisi. Mollit est incididunt sin aliqua duis. Deserunt ut velit deserunt fugiat eiusmod. Doincididunt laborum aliqua cupidatat adipisicing fugiat reprehenderit cillum id. Minim minim elit occaecat id velit fugiat ea. Aliqua excepteur ea excepteur cillum esse voluptate non elit laboris laboris esse est sunt incididunt ullamco.";
+    this.message = "Occaecat laboris incididunt ea labore quis in qui commodo velit cillum et commodo. Dolore consectetur eu eu reprehenderit anim fugiat in nostrud anim magna enim nisi. Mollit est incididunt sin aliqua duis. Deserunt ut velit deserunt fugiat eiusmod. Doincididunt laborum aliqua cupidatat adipisicing fugiat reprehenderit cillum id. Minim minim elit occaecat id velit fugiat ea. Aliqua excepteur ea excepteur cillum esse voluptate non elit laboris laboris esse est sunt incididunt ullamco.";
   }
 
   static get styles() {
@@ -30,35 +31,28 @@ export class AlertBlock extends LitElement {
         font-weight: 700;
         letter-spacing: 0.5px;
         line-height: 20px;
-        transition: 200ms 250ms all ease-in-out;
+        transition: all 200ms ease-in-out;
       }
+
       :host([open]) {
         --foreground-color: #ffd100;
-        display: inline-flex;
         background-color: var(--foreground-color);
-        width: 100%;
         min-height: 54px;
-        font-family: "Roboto", sans-serif;
-        font-size: 16px;
-        font-weight: 700;
-        letter-spacing: 0.5px;
-        line-height: 20px;
-        transition: 200ms 250ms all ease-in-out;
- }
+      }
 
       .alert {
-        max-height: 258;
-        display: inline-flex;
-        padding: 0px 62px 0px 62px;
+        display: flex;
         justify-content: space-between;
+        padding: 0px 62px;
+        position: relative;
+        max-height: 258px;
+      }
 
       .alert-content {
-        display: inline-flex;
         position: relative;
       }
 
       .alert-content::before {
-        display: inline-flex;
         content: " ";
         width: 0;
         height: 0;
@@ -70,9 +64,34 @@ export class AlertBlock extends LitElement {
         border-bottom: 30px solid var(--foreground-color);
       }
 
-      .paralellogram {
-        display: inline-flex;
-        position: relative;
+      .alert-icon {
+        padding: 20px 38px 20px 12px;
+        max-width: 46px;
+        max-height: 46px;
+        flex-grow: 0;
+        flex-shrink: 0;
+        stroke: var(--dark-text-color);
+        z-index: 1;
+      }
+
+      .alert-message {
+        margin: auto;
+        max-width: 930px;
+        min-width: 153px;
+        max-height: 258px;
+        font-family: "Roboto-Bold", sans-serif;
+        font-style: italic;
+        font-size: 18px;
+        letter-spacing: 0.5px;
+        line-height: 20px;
+        color: var(--dark-text-color);
+        z-index: 1;
+      }
+
+      .alert-date {
+        padding: 16px 4px;
+        margin: auto;
+        max-width: 150px;
       }
 
       .paralellogram::before {
@@ -88,37 +107,6 @@ export class AlertBlock extends LitElement {
         transform: skew(20deg);
         background-color: var(--foreground-color);
         z-index: 0;
-      }
-
-      .alert-date {
-        padding: 16px 50px 16px 4px;
-        margin: auto;
-        max-width: 150px;
-      }
-
-      .alert-icon {
-        max-width: 46px;
-        max-height: 46px;
-        padding: 20px 38px 20px 12px;
-        flex-grow: 0;
-        flex-shrink: 0;
-        stroke: var(--dark-text-color);
-        z-index: 1;
-      }
-
-      .alert-message {
-        max-width: 930px;
-        min-width: 153px;
-        max-height: 258px;
-        margin: auto;
-        font-family: "Roboto-Bold", sans-serif;
-        font-style: italic;
-        font-size: 18px;
-        letter-spacing: 0.5px;
-        line-height: 20px;
-        color: var(--dark-text-color);
-        text-size-adjust: 100%;
-        z-index: 1;
       }
 
       .alert-info {
@@ -152,4 +140,4 @@ export class AlertBlock extends LitElement {
   }
 }
 
-globalThis.customElements.define(AlertBlock.tag, AlertBlock);
+customElements.define(AlertBlock.tag, AlertBlock);
